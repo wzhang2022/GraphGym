c8cb07ffa49c3348b6ad90c591142ae17f8d8896
William Zhang <43010874+wzhang2022@users.noreply.github.com>
2022-03-22 15:45:30 -0400

    run script for baseline

---
+++ run/scripts/scaling_gnns/run_ogb.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+cd ../..
+
+DIR=scaling_gnns
+CONFIG=graph_ogb_test
+GRID=graph_ogb_test
+REPEAT=3
+MAX_JOBS=12
+SLEEP=1
+MAIN=main
+
+# generate configs (after controlling computational budget)
+# please remove --config_budget, if don't control computational budget
+python configs_gen.py --config configs/${DIR}/${CONFIG}.yaml \
+  --config_budget configs/${DIR}/${CONFIG}.yaml \
+  --grid grids/${DIR}/${GRID}.txt \
+  --out_dir configs
+# run batch of configs
+# Args: config_dir, num of repeats, max jobs running, sleep time
+bash parallel.sh configs/${CONFIG}_grid_${GRID} $REPEAT $MAX_JOBS $SLEEP $MAIN
+# rerun missed / stopped experiments
+bash parallel.sh configs/${CONFIG}_grid_${GRID} $REPEAT $MAX_JOBS $SLEEP $MAIN
+# rerun missed / stopped experiments
+bash parallel.sh configs/${CONFIG}_grid_${GRID} $REPEAT $MAX_JOBS $SLEEP $MAIN
+
+# aggregate results for the batch
+python agg_batch.py --dir results/${CONFIG}_grid_${GRID}
